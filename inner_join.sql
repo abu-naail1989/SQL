d7a22a3693a0fa8f9794c171decbe62d38a29eb6
James Alhassan <jamesalhassan052@gmail.com>
2022-01-10 04:09:14 +0000

    Add files via upload
    Initial Commit

---
+++ inner_join.sql
@@ -0,0 +1,15 @@
+SELECT a.primary_poc, w.occurred_at, w.channel, a.name
+FROM web_events w
+JOIN accounts a
+ON w.account_id = a.id
+WHERE a.name = 'Walmart';
+
+
+
+SELECT r.name region, s.name rep, a.name account
+FROM sales_reps s
+JOIN region r
+ON s.region_id = r.id
+JOIN accounts a
+ON a.sales_rep_id = s.id
+ORDER BY a.name;
