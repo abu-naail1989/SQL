0dcfd4ba6f9a8dcddab867fd72b9f4b10ac26969
James Alhassan <jamesalhassan052@gmail.com>
2022-01-24 06:05:43 +0000

    Add files via upload
    Initial Commit

---
+++ SUBQUERY_with_WHERE.sql
@@ -0,0 +1,21 @@
+SELECT 
+	-- Select the team long and short names
+	team_long_name,
+	team_short_name
+FROM team
+-- Exclude all values from the subquery
+WHERE team_api_id NOT IN
+     (SELECT DISTINCT hometeam_ID  FROM match);
+
+
+
+SELECT
+	-- Select the team long and short names
+	team_long_name,
+	team_short_name
+FROM team
+-- Filter for teams with 8 or more home goals
+WHERE team_api_id IN
+	  (SELECT  hometeam_ID
+       FROM match
+       WHERE home_goal >= 8);
